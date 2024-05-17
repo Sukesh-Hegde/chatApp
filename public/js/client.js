3d1c86e57c6b5a41aefd0f7847f12c56dcf59cda
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-05-17 12:25:37 +0530

    updated

--- public/js/client.js
+++ public/js/client.js
@@ -13,33 +13,27 @@ do{
     username=prompt("Enter your name: ");
 }while(!username);
 
-socket.emit("new-user-joined",username);//it will be passed to (io.on) in server.js file code
+socket.emit("new-user-joined",username);
 
-/* Notifying that user is joined */
-//recieving the event and storing it in variable (socket_name) that server send that someone is joined
 socket.on('user-connected',(socket_name)=>{
     userJoinLeft(socket_name,'joined');
 })
 
-/* Notifying that user is left */
-//recieves the event fron disconnected(server.js)
+
 socket.on("user-disconnected", (socket_name) => {
   userJoinLeft(socket_name, "left");
 });
 
-/* function to create joined/left status div */
 function userJoinLeft(name,status){
     let div=document.createElement('div');
     div.classList.add('user-join');
     let content = `<p><b>${name}</b> ${status} the chat</p>`;
-    div.innerHTML=content; //inserting the content insid the div
+    div.innerHTML=content; 
     chats.appendChild(div);
     chats.scrollTop = chats.scrollHeight;
 
 }
 
-/* For updating users list and user counts */
-/* updating user list upon new connection */
 socket.on('user-list',(users)=>{
     users_list.innerHTML="";
     users_arr=Object.values(users);
@@ -50,8 +44,6 @@ socket.on('user-list',(users)=>{
     }
     users_count.innerHTML = users_arr.length;
 })
-
-/* for sending message */
 
 msg_send.addEventListener("click", () => {
   let data = {
@@ -64,8 +56,8 @@ msg_send.addEventListener("click", () => {
     appendMessage(
       data,
       "outgoing"
-    ); /*sending data and a keyword named "outgoing" */
-    socket.emit("message", data); /* sending the data to server */
+    ); 
+    socket.emit("message", data); 
     user_msg.value = "";
   }
 });
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
-
 const server = http.createServer(app);
 const port = process.env.PORT || 8000;
 
@@ -12,26 +11,20 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// 2. Create socket server.
 const io = require("socket.io")(server);
 
-/* using socket events  */
 var users = {};
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (username) => {
-    users[socket.id] = username; //store the username with id
-    socket.broadcast.emit("user-connected", username); //broadcasting the message to everyone that someone is joined
-
-    /* upatating the list whnever the new connection (using io.emit because need to target every user)*/
+    users[socket.id] = username; 
+    socket.broadcast.emit("user-connected", username); 
     io.emit("user-list", users);
   });
 
-  /* when any user is disconnected */
   socket.on("disconnect", () => {
     socket.broadcast.emit("user-disconnected", (user = users[socket.id]));
     delete users[socket.id];
 
-    /* upatating the list whnever the new connection disconnets)*/
     io.emit("user-list", users);
   });
 
